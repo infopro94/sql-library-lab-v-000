132a15331b5f0d936c22ada8d188d345971406f1
Susan Fowler <infopro94@gmail.com>
2017-11-05 14:47:15 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,8 +8,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT "
-  #
+  "SELECT species, COUNT(*) FROM characters GROUP BY species ORDER BY COUNT (species) DESC LIMIT 1"
+  # tables with data: characters (count & name of highest # of species) total of? AS?
 end
 
 def select_name_and_series_subgenres_of_authors

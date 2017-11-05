57572e3af2c23afcf2b1550f0a4717da15f07ccf
Susan Fowler <infopro94@gmail.com>
2017-11-05 15:11:48 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,8 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series JOIN characters ON series.series_id = series.id"
+  #tables: series(title) characters(species=human) books(series_id, title)
 end
 
 def select_character_names_and_number_of_books_they_are_in
